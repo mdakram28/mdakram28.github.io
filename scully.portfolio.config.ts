b1f62a377e2b3cc45e468b4e2c06e2b8df81281b
Akram Ansari <mdakram28@gmail.com>
2020-11-13 06:52:16 +0530

    test

--- scully.portfolio.config.ts
+++ scully.portfolio.config.ts
@@ -2,7 +2,7 @@ import { ScullyConfig } from '@scullyio/scully';
 export const config: ScullyConfig = {
   projectRoot: "./src",
   projectName: "portfolio",
-  outDir: './dist/static',
+  outDir: './docs',
   routes: {
   }
 };
